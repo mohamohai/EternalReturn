762875ea67a034753d85fb6d91c1ea121d65094b
mohamohai <ahwjs1022@gmail.com>
2022-07-18 19:47:46 +0900

    css start

--- src/ER/Skeleton/ERMain.js
+++ src/ER/Skeleton/ERMain.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-
+import "./css/ERMain.css";
 class ERMain extends Component {
   constructor(props) {
     super(props);
@@ -8,7 +8,27 @@ class ERMain extends Component {
   }
 
   render() {
-    return <div className="ERMain"></div>;
+    return (
+      <div id="ERMain">
+        {/* <img
+          id="MainImg"
+          src="/image/Character_Img/Rio/Thumbnail/Default/Full.png"
+        /> */}
+        <form action="/Search_User/">
+          <input
+            className="SearchNickNameBox"
+            type="text"
+            name="NickName"
+            maxLength="50"
+            placeholder="닉네임"
+            onChange={(e) => this.NickNameAdd(e)}
+            onClick={this.conlog}
+            href={`Search_User/?NickName=${this.state.TextBoxNickName}`}
+          />
+          <a href={`Search_User/?NickName=${this.state.TextBoxNickName}`}></a>
+        </form>
+      </div>
+    );
   }
 }
 
