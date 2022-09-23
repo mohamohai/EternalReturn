922a4476cdbf559ff07d065c3f4386c1f51b7802
mohamohai <ahwjs1022@gmail.com>
2022-09-23 14:32:02 +0900

    flex test

--- src/VHOM/vhom.js
+++ src/VHOM/vhom.js
@@ -5,7 +5,6 @@ import { useState, useEffect, useRef } from "react";
 import "./VHOM.css";
 
 function VHOM() {
-
   const [SideBarText, setSideBarText] = useState('VHOM서비스소개');  
   const [SideBarVh, setSideBarVh] = useState(10);
   const [SideBarBorder, setSideBarBorder] = useState(SideBarVh+10);
@@ -17,15 +16,51 @@ function VHOM() {
           <a href="#">VHOM</a>
           <span>한솔이 만든 토탈 인테리어</span>
         </div>
-        <div className="VHOMGNBRight">zz</div>
+        <div className="VHOMGNBRight">
+          <div className="VHOMGNBUserInfo">
+            <ul>
+              <li>회원가입</li>
+              <li>고객센터</li>
+              <li>로그인</li>
+            </ul>
+          </div>
+          <br></br>
+          <div className="VHOMGNBMenu">
+            <ul className="VHOMGNBMenuRightThree">
+              <li><a>icon</a></li>
+              <li><a>icon</a></li>
+              <li><a>icon</a></li>
+            </ul>
+            <ul className="VHOMGNBMenuRightTwo">
+              <li><a>우리동네봄</a></li>
+              <li><a>스타일갤러리</a></li>
+            </ul>
+            <ul className="VHOMGNBMenuRightOne">
+              <li><a>이벤트 기획전</a>           </li>
+              <li><a>스토어</a>
+             
+              </li>
+              <li><a>VHOM나들이</a></li>
+              <li><a>토탈인테리어</a></li>
+            </ul>
+            
+        </div>
+        </div>
+        
       </div><div className="clear"></div>
       
       <div className="VHOMSideBarText" style={{top:`${SideBarVh}vh`}}>
         {SideBarText}
       </div>
       <div className="VHOMSideBarBorder" style={{height:`${SideBarBorder}vh`}}></div>
-      
-      
+      <details><summary>abc</summary>
+      <li>asdsadsad</li></details>
+      <div className="flexbox">
+        <div className="boax"></div>
+        <div className="boax"></div>
+        <div className="boax"></div>
+        <div className="boax"></div>
+      </div>
     </div>
     
   );
