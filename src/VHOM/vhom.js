e761a4da483e634a8216439be73a9fc422fd35c2
mohamohai <ahwjs1022@gmail.com>
2022-09-23 22:50:22 +0900

    sidebar move

--- src/VHOM/vhom.js
+++ src/VHOM/vhom.js
@@ -1,152 +1,172 @@
-
-
-
 import { useState, useEffect, useRef } from "react";
 import "./VHOM.css";
 
-
-
-
-function VHOM() {
+function Vhom() {
+ 
   const [SideBarText, setSideBarText] = useState('VHOM서비스소개');  
   const [SideBarVh, setSideBarVh] = useState(10);
-  const [SideBarBorder, setSideBarBorder] = useState(SideBarVh+10);
+  const [SideBarBorder, setSideBarBorder] = useState(20);
   const [Count, setCount] = useState(0); 
   
-const DivRef = useRef();
-const [scrollIndex, setScrollIndex] = useState(1);
-useEffect(() => {
-  const wheelHandler = (e) => {
-    const { deltaY } = e;
-    const { scrollTop } =DivRef.current; // 스크롤 위쪽 끝부분 위치
-    const pageHeight = window.innerHeight; // 화면 세로길이, 100vh와 같습니다.
+  const DivRef = useRef();
+  const [scrollIndex, setScrollIndex] = useState(1);
+
+  useEffect(() => {
+    const wheelHandler = (e) => {
   
-    console.log( scrollTop +"  "+ pageHeight)
-    if(deltaY > 0){
-      if(scrollTop<pageHeight)
-      {console.log("1p down")
+      const { deltaY } = e;
+      const { scrollTop } =DivRef.current; // 스크롤 위쪽 끝부분 위치
+      const pageHeight = window.innerHeight; // 화면 세로길이, 100vh와 같습니다.
+    
+      console.log( scrollTop +"  "+ pageHeight)
+      if(deltaY > 0){
+        if(scrollTop<pageHeight)
+        {console.log("1p down")
+          DivRef.current.scrollTo({
+          top: pageHeight *1,
+          left: 0,
+          behavior: "smooth",
+        });
+        setSideBarText("VHOM리모델링");
+        setSideBarVh(30);
+        setSideBarBorder(40)
+      }else if(scrollTop<pageHeight*2 && scrollTop>=pageHeight){
+        console.log("2p down")
         DivRef.current.scrollTo({
-        top: pageHeight *1,
+        top: pageHeight *2,
         left: 0,
         behavior: "smooth",
       });
-    }else if(scrollTop<pageHeight*2 && scrollTop>=pageHeight){
-      console.log("2p down")
-      DivRef.current.scrollTo({
-      top: pageHeight *2,
-      left: 0,
-      behavior: "smooth",
-    });
-    } else if(scrollTop<pageHeight*3 && scrollTop>=pageHeight*2){
-      console.log("3p down")
-      DivRef.current.scrollTo({
-      top: pageHeight *3,
-      left: 0,
-      behavior: "smooth",
-    });
-    }else if(scrollTop<pageHeight*4 && scrollTop>=pageHeight*3){
-      console.log("4p down")
-      DivRef.current.scrollTo({
-      top: pageHeight *4,
-      left: 0,
-      behavior: "smooth",
-    });
-  }
-    }else{
-      if(scrollTop>=pageHeight*4 )
-      { console.log("5p up")
+      setSideBarText("VHOM추천아이템");
+      setSideBarVh(50);
+      setSideBarBorder(60)
+      } else if(scrollTop<pageHeight*3 && scrollTop>=pageHeight*2){
+        console.log("3p down")
         DivRef.current.scrollTo({
         top: pageHeight *3,
         left: 0,
         behavior: "smooth",
       });
-    }else if(scrollTop>=pageHeight*3 && scrollTop<=pageHeight*4)
-    {console.log("4p up")
-      DivRef.current.scrollTo({
-      top: pageHeight * 2,
-      left: 0,
-      behavior: "smooth",
-    });
-    }else if(scrollTop>=pageHeight*2 && scrollTop<=pageHeight*3)
-    {console.log("3p up")
-      DivRef.current.scrollTo({
-      top: pageHeight * 1,
-      left: 0,
-      behavior: "smooth",
-    });
-    } 
-    else if(scrollTop>=pageHeight*1 && scrollTop<=pageHeight*2)
-    {console.log("2p up")
+      setSideBarText("VHOM스타일갤러리");
+      setSideBarVh(70);
+      setSideBarBorder(80)
+      }else if(scrollTop<pageHeight*4 && scrollTop>=pageHeight*3){
+        console.log("4p down")
+        DivRef.current.scrollTo({
+        top: pageHeight *4,
+        left: 0,
+        behavior: "smooth",
+      });
+      setSideBarText("CS Center");
+      setSideBarVh(90);
+      setSideBarBorder(100)
+    }
+      }else{
+        if(scrollTop>=pageHeight*4 )
+        { console.log("5p up")
+          DivRef.current.scrollTo({
+          top: pageHeight *3,
+          left: 0,
+          behavior: "smooth",
+        });setSideBarText("VHOM스타일갤러리");
+        setSideBarVh(70);
+        setSideBarBorder(80)
+      }else if(scrollTop>=pageHeight*3 && scrollTop<=pageHeight*4)
+      {console.log("4p up")
+        DivRef.current.scrollTo({
+        top: pageHeight * 2,
+        left: 0,
+        behavior: "smooth",
+      });
+      setSideBarText("VHOM추천아이템");
+      setSideBarVh(50);
+      setSideBarBorder(60)
+      }else if(scrollTop>=pageHeight*2 && scrollTop<=pageHeight*3)
+      {console.log("3p up")
+        DivRef.current.scrollTo({
+        top: pageHeight * 1,
+        left: 0,
+        behavior: "smooth",
+      });
+        setSideBarText("VHOM리모델링");
+        setSideBarVh(30);
+        setSideBarBorder(40)
+      } 
+      else if(scrollTop>=pageHeight*1 && scrollTop<=pageHeight*2)
+      {console.log("2p up")
+        DivRef.current.scrollTo({
+        top: pageHeight * 0,
+        left: 0,
+        behavior: "smooth",
+      });
+        setSideBarText("VHOM서비스소개");
+        setSideBarVh(10);
+        setSideBarBorder(20)
+      } 
+    };
+  }
+    const DivRefCurrent = DivRef.current;
+    DivRefCurrent.addEventListener("wheel", wheelHandler);
+    return () => {
+    };
+  }, []);
 
-      DivRef.current.scrollTo({
-      top: pageHeight * 0,
-      left: 0,
-      behavior: "smooth",
-    });
-    } 
-  };
-}
-  const DivRefCurrent = DivRef.current;
-  DivRefCurrent.addEventListener("wheel", wheelHandler);
-  return () => {
-  };
-}, []);
 
 
   return (
-    <div className="VHOM"ref={DivRef} >
+
+    <div className="VHOM" ref={DivRef}>
+      <div className="VHOMSideBarText" style={{top:`${SideBarVh}vh`}}>
+          {SideBarText} {/* 좌측바 */}
+        </div><div className="VHOMSideBarBorder" style={{height:`${SideBarBorder}vh`}}></div>
       <div className="VHOMGNB">
-        <div className="VHOMSideBarText" style={{top:`${SideBarVh}vh`}}>
-          {SideBarText}
-        </div>
-        <div className="VHOMSideBarBorder" style={{height:`${SideBarBorder}vh`}}></div>
         <div className="VHOMGNBLeft">
           <a href="#">VHOM</a>
           <span>한솔이 만든 토탈 인테리어</span>
         </div>
         <div className="VHOMGNBRight">
-          <div className="VHOMGNBUserInfo">
-            <ul>
-              <li className="">회원가입</li>
-              <li className="VHOMGNBUserInfoLine">고객센터</li>
-              <li className="VHOMGNBUserInfoLine">로그인</li>
-            </ul>
+          <div className="VHOMGNBRightMenu">
+            <div className="VHOMGNBRightUserMenu">
+              <ul>
+                <li>회원가입</li>
+                <li>고객센터</li>
+                <li>로그인</li>
+              </ul>
+            </div><br></br><br></br>
+            <div style={{margin:"10px 0 0 0"}}>
+            <div className="VHOMGNBRightMenuBarTwo">
+              <ul>
+                <li>우리동네봄</li>
+                <li>스타일갤러리</li>
+              </ul>
+            </div>
+            <div className="VHOMGNBRightMenuBarOne">
+              <ul>
+                <li>이벤트 / 기획전</li>
+                <li>스토어</li>
+                <li>VHOM나들이</li>
+                <li>토탈인테리어</li>
+              </ul>
+            </div>
+            </div>
+            
+            
+            
           </div>
-          <br></br>
-          <div className="VHOMGNBMenu">
-            <ul className="VHOMGNBMenuRightThree">
-              <li><a>icon</a></li>
-              <li><a>icon</a></li>
-              <li><a>icon</a></li>
-            </ul>
-            <ul className="VHOMGNBMenuRightTwo">
-              <li><a>우리동네봄</a></li>
-              <li><a>스타일갤러리</a></li>
-            </ul>
-            <ul className="VHOMGNBMenuRightOne">
-              <li><a>이벤트 기획전</a></li>
-              <li><a>스토어</a></li>
-              <li><a>VHOM나들이</a></li>
-              <li><a>토탈인테리어</a></li>
-            </ul>
         </div>
-        </div>
-        
-      </div><div className="clear"></div><br></br>
-      <div className="VHOMMainPageOne   page">1</div>
+      </div>
+      <div className="clear"></div><br></br>
+      <div className="VHOMMainPageOne  page">
+       <div>
+        <li></li>
+       </div>
+      </div>
       <div className="VHOMMainPageTwo   page">2</div>
       <div className="VHOMMainPageThree page">3</div>
       <div className="VHOMMainPageFour  page">4</div>
-      
-      
-      <details>
-        <summary>abc</summary>
-          <li>asdsadsad</li>
-      </details>
-      
     </div>
     
   );
 }
 
-export default VHOM;
+export default Vhom;
