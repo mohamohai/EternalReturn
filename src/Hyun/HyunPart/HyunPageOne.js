6cb30fc24bc0ab9f9b8306297e0a3cfe01e9bd06
mohamohai <ahwjs1022@gmail.com>
2022-12-15 03:43:25 +0900

    pull IK

--- src/Hyun/HyunPart/HyunPageOne.js
+++ src/Hyun/HyunPart/HyunPageOne.js
@@ -2,9 +2,35 @@ import { useState, useEffect, useRef, Component } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "./css/HyunPageOne.css"
+import "./css/HyunPageOne.scss"
 
 function HyunPageOne(){
-    return(<div className="HyunPageOne">
-        
-    </div>)
+
+    
+    return(
+        <div className="HyunPageOne">
+            <div>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+                <div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/><div className="FallingSnow"/>
+            </div>
+            <div className="ikmyeong"></div>
+        </div>)
 }export default HyunPageOne;
