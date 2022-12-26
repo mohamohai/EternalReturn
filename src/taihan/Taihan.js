09c166d3bb6d09d4708b37515d57c5642915e955
mohamohai <ahwjs1022@gmail.com>
2022-12-26 19:22:21 +0900

    javascript memory heap swiper..?

--- src/taihan/Taihan.js
+++ src/taihan/Taihan.js
@@ -24,9 +24,53 @@ function Taihan(){
     return(
     <div className="taihanMain">
         <TaihanGNB></TaihanGNB>
-        asd
-        javascript memory heap
-        oh my god memory
+        
+  <Swiper
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        spaceBetween={0}
+        effect={"fade"}//전환효과
+        loop = {"false"} // 슬라이드 반복 여부
+        autoplay={{ delay: 4000, }}
+        modules={[EffectFade,Autoplay,Pagination]}
+        onSlideChange={() => console.log('slide change')}
+        className="mySwiper"
+      >
+
+        <SwiperSlide>
+
+        <div className="TaihanPageOne" data-swiper-autoplay="4000">
+       
+        <div className="TainhanPageOneTitle" data-aos="fade-up">asdasasdsadasdasdd <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>asdasdasdsadsadsad</div>
+        <div>asdasd</div>
+        <div>asdasd</div>
+
+            <video className="TaihanVideo" preload="metadata" muted playsInline autoPlay loop>
+                <source src={`Video/Taihan/Taihan1.mp4`} type="video/mp4"/>
+            </video>
+
+        </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+        <div className="TaihanPageOne" data-swiper-autoplay="4000">
+
+
+            <video preload="metadata" muted playsInline autoPlay loop>
+                <source src={`Video/Taihan/Taihan2.mp4`} type="video/mp4"/>
+            </video>
+        </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+        
+        <div className="TaihanPageOne" data-swiper-autoplay="5000">
+            <video preload="metadata" muted playsInline autoPlay loop>
+                <source src={`Video/Taihan/Taihan3.mp4`} type="video/mp4"/>
+            </video>
+        </div>
+        </SwiperSlide>
+      </Swiper>
        
     </div>)
 }export default Taihan
